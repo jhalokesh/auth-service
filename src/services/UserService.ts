8f80f05303c10cde65a974d9fe09fd05119e61ce
jhalokesh <lokeshkumarjha07+github@gmail.com>
2024-12-04 20:14:30 +0530

    Refactor: Use role constant for customer role assignment
    Replace hardcoded 'customer' string with Roles.CUSTOMER constant for better maintainability and type safety

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,6 +2,7 @@ import { Repository } from 'typeorm';
 import { User } from '../entity/User';
 import { UserData } from '../types';
 import createHttpError from 'http-errors';
+import { Roles } from '../constants';
 
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
@@ -13,7 +14,7 @@ export class UserService {
                 lastName,
                 email,
                 password,
-                role: 'customer',
+                role: Roles.CUSTOMER,
             });
             // eslint-disable-next-line @typescript-eslint/no-unused-vars
         } catch (err) {
